843727d20df0dc5555966489a90a93042404ca6b
Horixon127 <DaviidRodriguez777@hotmail.com>
2021-05-05 17:50:11 -0500

    ProgresoJavaScript

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
-function imprimir(){
-
-    let impresion = ""
-    impresion = document.getElementById("name").value + "\n"
-    impresion += document.getElementById("lastname").value + "\n"
-    impresion += document.getElementById("age").value + "\n"
-    impresion += document.getElementById("course").value + "\n"
-
-    alert(impresion)
-    console.log("hola" + "a" + "todos")
+function imprimir() {
+  let impresion = "";
+  impresion = document.getElementById("name").value + "\n";
+  impresion += document.getElementById("lastname").value + "\n";
+  impresion += document.getElementById("age").value + "\n";
+  impresion += document.getElementById("course").value +  "\n";
+  alert(impresion);
+  console.log("hola" + "a" + "todos");
 }
 
-    console.log("funcionando")
+console.log("funcionando");
---
+++ js/script3.js
@@ -0,0 +1,23 @@
+function Estudiantes1() {
+  let estudiante = document.getElementById("respuesta").value;
+
+  if (estudiante === "presente") {
+    alert("El Estudiante Esta Presente");
+  } else {
+    alert("El Estudiante No Asistio");
+  }
+}
+
+function Estudiantes2() {
+  let Presente = false;
+  let No_Presente = false;
+
+  if (Presente === true) {
+    alert("El Estudiante Esta ");
+  } //accion a ejecutar si la condicion no se cumple
+
+  if (No_Presente === false) {
+    alert("El Estudiante No Esta");
+  }
+}
+ 

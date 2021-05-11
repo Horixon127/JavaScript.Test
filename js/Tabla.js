23b7c312695fae5e8b94281a7032afe632c24a3c
Horixon127 <DaviidRodriguez777@hotmail.com>
2021-05-11 15:33:59 -0500

    PruebaTabla

---
+++ js/Tabla.js
@@ -0,0 +1,33 @@
+for (let i = 2; i <= 20; i = i + 2) {
+  let = table` <table class="table table-striped table-dark">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">${i}</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+    <tr>
+      <th scope="row">${i += 2}</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">${i += 2}</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    </tbody>
+  </table>`;
+  document.getElementById("contenedor-Enfacis").innerHTML += table;
+}
